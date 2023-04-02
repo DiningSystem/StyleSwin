bacade9b279168769a0722e016fcef8194b6392c
Emkay Nguyen <81498410+DiningSystem@users.noreply.github.com>
2023-04-02 18:07:39 -0500

    Update fid_score.py

--- utils/fid_score.py
+++ utils/fid_score.py
@@ -120,7 +120,7 @@ def get_activations(files, model, batch_size=50, dims=2048, device='cpu', num_wo
     
     transform = TF.Compose(
             [
-                TF.Resize((512, 512)),
+                TF.Resize((1024, 1024)),
                 
                 TF.ToTensor()
             ]
